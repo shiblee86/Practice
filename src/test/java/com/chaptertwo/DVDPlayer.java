171eb54f9579d9f7676f2488fcc9f0eed567a8d7
Syed Abdullah <syed.r.abdullah@gmail.com>
2019-01-09 21:03:58 -0500

    New examples.

---
+++ src/test/java/com/chaptertwo/DVDPlayer.java
@@ -0,0 +1,9 @@
+package com.chaptertwo;
+
+public class DVDPlayer {
+
+	public DVDPlayer() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- src/test/java/com/chaptertwo/TapeDeck.java
+++ src/test/java/com/chaptertwo/TapeDeck.java
@@ -28,3 +28,33 @@ class TapeDeckTestDrive {
 		}
 	}
 }
+
+class DVDPlayer {
+
+	boolean canRecord = false;
+	
+	void recordDVD() {
+		System.out.println("DVD recording");
+	}
+	
+	void playDVD() {
+		System.out.println("DVD Playing");
+	}
+
+}
+
+class DVDPlayerTestDrive {
+	
+	public static void main(String[] args) {
+		
+		DVDPlayer d = new DVDPlayer();
+		
+		d.canRecord = true;
+		d.playDVD();
+		
+		if(d.canRecord == true) {
+			d.recordDVD();
+		}
+		
+	}
+}
